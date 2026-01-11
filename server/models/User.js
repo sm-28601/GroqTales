196d186a4bc859ac3e38c35f9069b4c65ddda846
Sourya <souryaadhikary2911@gmail.com>
2026-01-11 21:28:29 +0530

    Added user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
-    // will be added later
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true, required: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    phone: { type: String, default: null },
+    location: { type: String, default: null },
+    role: {
+      type: String,
+      enum: ["ngo", "doctor", "health_worker", "patient"],
+      required: true,
+    }
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10); // bcrypt rounds = 10
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
-const User = mongoose.model('User',UserSchema);
 
-module.exports = User;
+UserSchema.methods.comparePassword = function (plain) {
+  return bcrypt.compare(plain, this.password);
+};
+
+module.exports =  mongoose.model("User", UserSchema);
